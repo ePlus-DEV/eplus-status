43f8192ee068b2eb4ae5e4340c91fab88c3a85d4
David Nguyen <h250694@gmail.com>
2023-08-26 23:45:15 +0700

    add Tracker chart

--- app/page.tsx
+++ app/page.tsx
@@ -23,31 +23,46 @@ type ApiResponse = {
 };
 
 const Home = () => {
-  const [data, setData] = useState<ApiResponse | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [data1, setData1] = useState<ApiResponse | null>(null);
+  const [data2, setData2] = useState<ApiResponse | null>(null);
+  const [loading1, setLoading1] = useState(true);
+  const [loading2, setLoading2] = useState(true);
+  const [error1, setError1] = useState("");
+  const [error2, setError2] = useState("");
 
   useEffect(() => {
-    setLoading(true);
-    fetch("https://api.freshping.io/v1/public-check-response-times-reports/690040/?for_hours=48&aggregate_by=hours")
-      .then(async (res) => {
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
+     Promise.all([
+      fetch("https://api.freshping.io/v1/public-check-response-times-reports/873076/?for_hours=24&aggregate_by=hours"),
+      fetch("https://api.freshping.io/v1/public-check-response-times-reports/873076/?for_hours=720&aggregate_by=days"),
+    ])
+      .then(async (responses) => {
+        // Chuyển đổi các phản hồi thành dữ liệu JSON
+        const [response1, response2] = responses;
+        if (!response1.ok) {
+          throw new Error(`HTTP error! Status: ${response1.status}`);
         }
-        const apiResponse: ApiResponse = await res.json();
-        setData(apiResponse);
+        if (!response2.ok) {
+          throw new Error(`HTTP error! Status: ${response2.status}`);
+        }
+        const data1: ApiResponse = await response1.json();
+        const data2: ApiResponse = await response2.json();
+
+        // Cập nhật state với dữ liệu từ cả hai API
+        setData1(data1);
+        setData2(data2);
       })
-      .catch((e) => {
-        if (e instanceof Error) {
-          setError(e.message);
-        }
+      .catch((error) => {
+        // Xử lý lỗi nếu có
+        setError1(error.message);
+        setError2(error.message);
       })
       .finally(() => {
-        setLoading(false);
+        setLoading1(false);
+        setLoading2(false);
       });
   }, []);
 
-  const errorComponent = <div className="text-red-500">Error: {error}</div>;
+
   const dataFormatter = (number: number) => {
   return number + " ms";
   };
@@ -55,24 +70,27 @@ const Home = () => {
   return (
     <Card>
     <Title>ePlus.DEV - Performance metrics</Title>
-      {loading ? (
-        <SkeletonCard />
-      ) : error ? (
-        errorComponent
+      {loading1 || loading2 ? (
+        <SkeletonCard/>
+      ) : error1 || error2 ? (
+        <div className="text-red-500">Error: {error1 || error2}</div>
       ) : (
-        <div>             
-            <AreaChart
+        <div>
+          <AreaChart
             className="h-72 mt-4"
-            data={data?.response_times.map((responseTime) => ({
+            data={data1?.response_times.map((responseTime) => ({
               date: dayjs(responseTime.start).format("HH:mm"),
               "Response Time": responseTime.average_response_time_milliseconds_available,
-              // "The Pragmatic Engineer": responseTime.duration_seconds_available,
-            })) as any} // Sử dụng "as any" để ghi đè kiểm tra kiểu dữ liệu
+            })) as any}
             index="date"
             categories={["Response Time"]}
             colors={["indigo", "cyan"]}
             valueFormatter={dataFormatter}
-          />
+              />
+            <Tracker data={data2?.response_times.map((responseTime) => ({
+              color: responseTime.average_response_time_milliseconds_available !== null ? "emerald" : "rose",
+              tooltip: responseTime.average_response_time_milliseconds_available !== null ? dayjs(responseTime.start).format("DD/MM/YYYY") +" - Operational" : dayjs(responseTime.start).format("DD/MM/YYYY") +" - Downtime"
+            })) as any} className="mt-2" />
         </div>
       )}
     </Card>
